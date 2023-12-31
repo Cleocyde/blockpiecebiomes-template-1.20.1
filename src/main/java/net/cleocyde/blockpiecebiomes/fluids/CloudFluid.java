fa88141edc58376c00dd44a6c626a2cfedd31cd6
Cleocyde <cleocyde@outlook.fr>
2023-12-31 09:56:52 +0100

    Cloud Fluid doesn't flow anymore

--- src/main/java/net/cleocyde/blockpiecebiomes/fluids/CloudFluid.java
+++ src/main/java/net/cleocyde/blockpiecebiomes/fluids/CloudFluid.java
@@ -8,7 +8,6 @@ import net.minecraft.item.Item;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 
